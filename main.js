cbaef880aeac2226d4201acd8a1bd455e4f7262b
Ivsels <selser.ivan@gmail.com>
2023-10-17 15:13:51 -0300

    modificado el texto de Alert inicial

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
-alert("Por favor lea las intrucciones sobre el uso de este aplicativo.");
+alert(`Por favor lea las intrucciones sobre el uso de este aplicativo.
+
+1- Elija el tipo de Dolar (U$D) que desea utilizar.
+2- Indique el monto, en Dolares (U$D), a convertir en su valor en Pesos ($).
+3- Los cálculos aparecerán en la caja de texto.`);
 
 const dolarOficial = 365.5; // Valor fijado por el Bco. Central
 const pais = 30;
@@ -22,9 +26,10 @@ do {
 
         do{
             montoDolar = parseFloat(prompt("Indique el valor en dolares U$D que quiere convertir a pesos: "));
-        }while (montoDolar == NaN);
+        }while (isNaN(montoDolar));
 
         if (tipoDolar === "qatar") {
+            console.log("Qatar");
             let valorBase = dolarOficial * montoDolar;
             let impPais = valorBase * pais / 100;
             let impGanancias = valorBase * ganancias / 100;
@@ -36,6 +41,7 @@ do {
         }
 
         else if (tipoDolar === "turista") {
+            console.log("Turista");
             let valorBase = dolarOficial * montoDolar;
             let impPais = valorBase * pais / 100;
             let impGanancias = valorBase * ganancias / 100;
@@ -46,18 +52,18 @@ do {
         }
 
         else if (tipoDolar === "mep") {
-            console.log()
-            let valorBase = dolarOficial * montoDolar;
-            let impPais = valorBase * pais / 100;
-            let impGanancias = valorBase * ganancias / 100;
-            let total = valorBase + impPais + impGanancias;
-            textoFinal = "CALCULO DOLAR vs. PESOS (Turista/Tarjeta)\n\nDolares: " + montoDolar + " U$D\n\nDolar Oficial: $" + valorBase + "\nImpuesto PAIS (30%): $" + impPais + "\nImpuesto Ganancias (45%): $" + impGanancias + "\n\nTOTAL: $" + total;
+            console.log("MEP");
+            valorFinal = dolarMEP * montoDolar;
+            textoFinal = "CALCULO DOLAR vs. PESOS (MEP)\n\nDolares: " + montoDolar + " U$D\n\nCotización MEP: $" + dolarMEP + "\nBono operado: AL30" + "\n\nTOTAL: $" + valorFinal;
             cuadrito.value = textoFinal;
             correcto = true;
         }
 
         else  { // CCL
-            console.log("ccl");
+            console.log("CCL");
+            valorFinal = dolarCCL * montoDolar;
+            textoFinal = "CALCULO DOLAR vs. PESOS (CCL)\n\nDolares: " + montoDolar + " U$D\n\nCotización CCL: $" + dolarCCL + "\nBono operado: GD30" + "\n\nTOTAL: $" + valorFinal;
+            cuadrito.value = textoFinal;
             correcto = true;
         }
     }
